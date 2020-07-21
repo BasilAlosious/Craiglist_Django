18550fb87d18e2671bedb93cd6e0cbf91c0eb21f
Basil Alosious <basilalosious@gmail.com>
2020-07-21 12:59:03 +0530

    Frontend and requirements.txt added successfully.

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,16 @@
+import requests
 from django.shortcuts import render
+from bs4 import Beautifulsoup
 
 # Create your views here.
 def home(request):
     return render(request,'base.html')
+
+
+def new_search(request):
+    # Search gets the post request made . ie why post.get. 'search is name of placeholder'
+    search= request.POST.get('search')
+
+    frontend= {'search':search,
+               }
+    return render(request,'my_app/new_search.html',frontend)
